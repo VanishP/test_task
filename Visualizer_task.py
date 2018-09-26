697c84f30abf96be87eea17dea9ed38ba85a8155
Ivan Popov <blinkomom@yandex.ru>
2018-09-27 02:23:36 +0300

    add command line arguments parser

--- Visualizer_task.py
+++ Visualizer_task.py
@@ -4,12 +4,12 @@ Creates plots of value and average price
 
 """
 
-import plotly.plotly as py
+import sys
+import argparse
 import json
 
-def parse_file(file_name):
+def parse_file(file):
     """ Parses .json file into argument dictionaries, return dict with lists of arguments """
-    json_data = open(file_name)
 
     # Define argument lists for json.load
     def arg_to_lists(list_of_dict: object) -> object:
@@ -19,14 +19,46 @@ def parse_file(file_name):
         return dict(zip(["time_stamp", "volume", "average_price"], [ts, volume, av_price]))
 
     try:
-        args = json.load(json_data)
+        args = json.load(file)
         return arg_to_lists(args)
     except:
         print('Error: incorrect structure of JSON file')
-        return False
+        sys.exit(1)
+
+def parser_cl_args():
+    """ Parses arguments from command line, return required arguments"""
+    if len(sys.argv) == 10 and sys.argv[1] == "create":
+        cl_arg =  parse_create_mode_cl_args()
+    elif len(sys.argv) == 9 and sys.argv[1] == "append":
+        cl_arg = parse_append_mode_cl_args()
+    else:
+        print("Error: incorrect arguments or their count")
+        sys.exit(1)
+    return cl_arg
+
+def parse_create_mode_cl_args():
+    """ Return required arguments for create mode"""
+    pars = argparse.ArgumentParser()
+    pars.add_argument("mode")
+    pars.add_argument("data_file", type=argparse.FileType())
+    pars.add_argument("--interval", type=int)
+    pars.add_argument("--output", type=str)
+    pars.add_argument("--period", nargs=2, type=float)
+    cl_arg =  pars.parse_args()
+    return cl_arg
+
+def parse_append_mode_cl_args():
+    """" Return required arguments for create mode"""
+    pars = argparse.ArgumentParser()
+    pars.add_argument("mode")
+    pars.add_argument("old_file", type=argparse.FileType())
+    pars.add_argument("new_file", type=argparse.FileType())
+    pars.add_argument("--output", type=str)
+    pars.add_argument("--period", nargs=2, type=float)
+    cl_arg = pars.parse_args()
+    return cl_arg
 
 
 
 
-json_data1 = parse_file("TKN0_TKN88.json")
-print(json_data1["average_price"])
+
