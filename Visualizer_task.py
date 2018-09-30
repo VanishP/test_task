e0e9f6e53898b4082d7f9985ffcd880d4e6bd6d6
Ivan Popov <blinkomom@yandex.ru>
2018-10-01 02:46:26 +0300

    conformity with require PEP8

--- Visualizer_task.py
+++ Visualizer_task.py
@@ -5,26 +5,38 @@ Creates plots of value and average price
 """
 
 import sys
-import datetime
-from typing import Dict, List, io, ClassVar
 from math import pi
 import argparse
 import json
+import shutil
+from typing import Dict, List, io, ClassVar
+
+import datetime
+
+import pathlib
+
 import bokeh.layouts as layouts
 from bokeh.io import output_file, show
 from bokeh.plotting import figure
-import shutil
-import pathlib
 import pandas as pd
-import numpy as np
+
+
+class MyError(Exception):
+    def __init__(self,value):
+        self.msg = value
+    def __str__(self):
+        return self.msg
+
+
 
 def parse_file(json_file: io) -> Dict[str, List]:
     """
     Parses .json file into argument dictionaries,
     return dict with lists of arguments
     """
-    print("parse_file")
-    # Define argument lists for json.load
+    print("in parse_file")
+
+    # Define dict of argument lists for json.load
     def data_to_lists(list_of_dict: List[Dict]) -> Dict[str, List]:
         ts = list(map(lambda obj: float(obj["ts"]), list_of_dict))
         date = list(map(lambda obj:
@@ -35,13 +47,11 @@ def parse_file(json_file: io) -> Dict[str, List]:
         return dict(zip(["date", "time_stamp", "volume", "average_price"],
                         [date, ts, volume, av_price]))
 
-    try:
-        args = json.load(json_file)
-        json_file.close()
-        return data_to_lists(args)
-    except ValueError :
-        print('Error: incorrect structure of JSON file')
-        sys.exit(1)
+    args = json.load(json_file)
+    json_file.close()
+    return data_to_lists(args)
+
+
 
 def parser_cl_args() -> ClassVar:
     """ Parses arguments from command line, return required arguments"""
@@ -68,67 +78,81 @@ def parser_cl_args() -> ClassVar:
     pars.add_argument("--period", nargs=4, type=str,
                       help="""time period for build plots in
                            ISO 8601 format (YYYY-MM-DDTHH:MM:SS.mmmmmm""")
+
+
+    cl_arg =  pars.parse_args()
     try:
-        cl_arg =  pars.parse_args()
-    except FileExistsError:
-        print("Error: file or directory not found")
+        if cl_arg.mode == "create" and cl_arg.new_file  is not None:
+            raise MyError("Error: arg new_file isn't required in this mode ")
+    except MyError as err:
+        print(err.msg)
         sys.exit(1)
-    if cl_arg.mode == "create" and cl_arg.new_file != None:
-        print("Error: argument new_file isn't required in this mode ")
-        sys.exit(1)
-    elif cl_arg.mode == "append" and cl_arg.new_file == None:
-        print("Error:  argument new_file are required in this mode ")
-        sys.exit(1)
-    if cl_arg.interval[0] not in [1, 2, 3, 4, 5]:
-        print("Error: incorrect interval time index")
-        sys.exit(1)
-    print(cl_arg.period)
     try:
-        fmt = "%Y-%m-%d %H:%M:%S.%f"
-        p1 = cl_arg.period[0] + " " + cl_arg.period[1]
-        p2 = cl_arg.period[2] + " " + cl_arg.period[3]
-        print(datetime.datetime.strptime(p1,fmt))
-        if  datetime.datetime.strptime(p1,fmt) > \
-            datetime.datetime.strptime(p2,fmt):
-            print("Error: incorrect period")
-            sys.exit(1)
-    except:
-        print("Error: incorrect period date format")
+        if cl_arg.mode == "append" and cl_arg.new_file is None:
+            raise MyError("Error: arg new_file are required in this mode ")
+    except MyError as err:
+        print(err.msg)
+        sys.exit(1)
+    try:
+        if cl_arg.interval[0] not in [1, 2, 3, 4, 5]:
+            raise MyError("Error: incorrect interval time index")
+    except MyError as err:
+        print(err.msg)
+        sys.exit(1)
+    fmt = "%Y-%m-%d %H:%M:%S.%f"
+    p1 = cl_arg.period[0] + " " + cl_arg.period[1]
+    p2 = cl_arg.period[2] + " " + cl_arg.period[3]
+    try:
+        if  (datetime.datetime.strptime(p1,fmt) >
+             datetime.datetime.strptime(p2,fmt)):
+            raise MyError("Error: incorrect period")
+    except MyError as err:
+        print(err.msg)
         cl_arg.period = None
     return cl_arg
+
 
 def manage_visualizer(cl_arg: ClassVar):
     """
     Creates files with plots and data in selected mode(create, append) and
     records data and plots in dir.
     """
-    time_delta = {1: datetime.timedelta(seconds=cl_arg.interval[1]),
+    time_delta = {
+             1: datetime.timedelta(seconds=cl_arg.interval[1]),
              2: datetime.timedelta(minutes=cl_arg.interval[1]),
              3: datetime.timedelta(hours=cl_arg.interval[1]),
              4: datetime.timedelta(days=cl_arg.interval[1]),
-             5: datetime.timedelta(weeks=cl_arg.interval[1])}
+             5: datetime.timedelta(weeks=cl_arg.interval[1])
+             }
     print("in manage_visualizer")
+
     if cl_arg.mode == "create":
         data_frame = pd.DataFrame(parse_file(cl_arg.file))
         data_frame = data_frame.sort_values(by="date")
-        if cl_arg.period != None:
+        if cl_arg.period is not None:
             data_for_plots = choise_date_in_period(cl_arg.period,
-                                               data_frame)
+                                                   data_frame)
         else:
             data_for_plots = data_frame
         plots = create_plots(data_for_plots, time_delta[cl_arg.interval[0]])
         record_in_dir(cl_arg.file, cl_arg.output, plots)
+
     else:
-        data = cl_arg.file.read()[:-1] + ", " + cl_arg.new_file.read()[1:]
-        cl_arg.file.close()
-        cl_arg.new_file.close()
         data_file = pathlib.Path.cwd() / (cl_arg.output + "_data.json")
-        out_data = open(cl_arg.output + "_data.json", "w+")
-        out_data.write(data)
-        out_data.seek(0)
+        out_data = open(cl_arg.output + "_data.json", "w")
+        data2 = cl_arg.new_file.read()
+        data2 = data2[:-1] + ","
+        cl_arg.new_file.close()
+        out_data.write(data2)
+        data1 = cl_arg.file.read()
+        data1 = data1[1:]
+        cl_arg.file.close()
+        out_data.write(data1)
+        out_data.close()
+        out_data = open(cl_arg.output + "_data.json", "r")
         data_frame = pd.DataFrame(parse_file(out_data))
         data_frame = data_frame.sort_values(by="date")
-        if cl_arg.period != None:
+        if cl_arg.period is not None:
             data_for_plots = choise_date_in_period(cl_arg.period,
                                                    data_frame, data_file)
         else:
@@ -146,10 +170,12 @@ def choise_date_in_period(period: List[str],
     fmt = "%Y-%m-%d %H:%M:%S.%f"
     p0 = datetime.datetime.strptime(period[0] + " " + period[1],fmt)
     p1 = datetime.datetime.strptime(period[2] + " " + period[3],fmt)
-    i = 0
     length = data_frame.shape[0]-1
-    if p0 > data_frame.values[length, 0] or p1 < data_frame.values[0, 0]:
-        print("Error: this period doesn't contain any values")
+    try:
+        if p0 > data_frame.values[length, 0] or p1 < data_frame.values[0, 0]:
+           raise MyError("Error: this period doesn't contain any values")
+    except MyError as err:
+        print(err.msg)
         data_file.unlink()
         sys.exit(1)
     data_frame = data_frame.loc[data_frame["date"] >= p0]
@@ -157,37 +183,37 @@ def choise_date_in_period(period: List[str],
     return data_frame
 
 def record_in_dir(data_file: io, output: str, plots: ClassVar):
-    """ records in dir plots and data"""
+    """ Records in dir plots and data"""
     print("in record_in_dir")
     directory = pathlib.Path.cwd()/(output + "_dir")
-    try:
-        directory.mkdir(parents=True)
-    except FileExistsError:
-         print("Error: directory with graph already exist")
-         sys.exit(1)
+    directory.mkdir(parents=True)
     output_file(directory/(output + ".html"))
     show(plots)
     shutil.copyfile(data_file.name, directory/(output + "_data.json"))
     data_file.close()
+
 
 def create_plots(data_frame: ClassVar, time_delta: ClassVar) -> ClassVar:
     """ Create plots of volume and average price """
     print("in create_plots")
 
     plot1 = figure(title=" Volume plot", x_axis_label="time stamp",
-                   y_axis_label = "volume",  x_axis_type="datetime")
+                   y_axis_label = "volume",  x_axis_type="datetime",
+                   plot_width=1000)
     plot1.line(data_frame["date"], data_frame["volume"])
     plot1.yaxis.major_label_orientation = "vertical"
+    plot1.xaxis[0].formatter.days = '%d\n%b\n%Y'
+    plot1.xaxis.major_label_orientation = pi / 3
     plot2 = figure(title=" Average price plot", x_axis_label="time stamp",
                    y_axis_label = "average price", x_axis_type="datetime",
-                   x_minor_ticks=3)
-    plot2.xaxis[0].formatter.days = '%d/%m/%Y'
+                   x_minor_ticks=3,  plot_width=1000)
+    plot2.xaxis[0].formatter.days = '%d/%b/%Y'
     plot2.xaxis.major_label_orientation = pi / 3
     plot2.line(data_frame["date"], data_frame["average_price"])
     plot2.yaxis.major_label_orientation = "vertical"
     return layouts.column([plot1, plot2])
 
 
-
-cl_arg = parser_cl_args()
-manage_visualizer(cl_arg)
+if __name__ == '__main__':
+    cl_arg = parser_cl_args()
+    manage_visualizer(cl_arg)
